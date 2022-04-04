f3a641102c7f202fb0462068e0f87e6d70437da8
Alexandrescu Andrei-Robert <ar_alexandrescu@yahoo.com>
2022-04-04 09:48:29 +0300

    Integration tests

--- src/test/java/service/ServiceIntegrationTest.java
+++ src/test/java/service/ServiceIntegrationTest.java
@@ -4,13 +4,18 @@ import domain.Assignment;
 import domain.Grade;
 import domain.Student;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import repository.*;
-import validation.*;
+import repository.AssignmentRepository;
+import repository.GradeRepository;
+import repository.StudentRepository;
+import validation.AssignmentValidator;
+import validation.GradeValidator;
+import validation.StudentValidator;
+import validation.Validator;
 
-public class ServiceTest {
+public class ServiceIntegrationTest {
     private static final Validator<Student> studentValidator = new StudentValidator();
     private static final Validator<Assignment> assignmentValidator = new AssignmentValidator();
     private static final Validator<Grade> gradeValidator = new GradeValidator();
@@ -31,155 +36,75 @@ public class ServiceTest {
     }
 
     @Test
-    public void testSaveStudentTC1() {
+    public void testSaveStudent() {
         //Arrange
-        String id = "";
-        String name = "Andrei";
-        int group = 110;
-        int expected = 1;
-
-        //Act
-        int actual = service.saveStudent(id, name, group);
-
-        //Assert
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSaveStudentTC2() {
-        //Arrange
-        String id = null;
-        String name = "Andrei";
-        int group = 110;
-        int expected = 1;
-
-        //Act
-        int actual = service.saveStudent(id, name, group);
-
-        //Assert
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSaveStudentTC3() {
-        //Arrange
-        String id = "1";
-        String name = "";
-        int group = 110;
-        int expected = 1;
-
-        //Act
-        int actual = service.saveStudent(id, name, group);
-
-        //Assert
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSaveStudentTC4() {
-        //Arrange
-        String id = "1";
-        String name = null;
-        int group = 110;
-        int expected = 1;
-
-        //Act
-        int actual = service.saveStudent(id, name, group);
-
-        //Assert
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSaveStudentTC5() {
-        //Arrange
-        String id = "1";
-        String name = "Andrei";
-        int group = 109;
-        int expected = 1;
-
-        //Act
-        int actual = service.saveStudent(id, name, group);
-
-        //Assert
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSaveStudentTC6() {
-        //Arrange
-        String id = "1";
-        String name = "Andrei";
-        int group = 939;
-        int expected = 1;
-
-        //Act
-        int actual = service.saveStudent(id, name, group);
-
-        //Assert
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSaveStudentTC7() {
-        //Arrange
-        String id = "100";
-        String name1 = "Andrei";
-        int group1 = 111;
-        String name2 = "Dragos";
-        int group2 = 932;
-
-        //Act
-        int res1 = service.saveStudent(id, name1, group1);
-        int res2 = service.saveStudent(id, name2, group2);
-
-        //Assert
-        Assertions.assertEquals(0, res1);
-        Assertions.assertEquals(1, res2);
-    }
-
-    @Test
-    public void testSaveStudentTC8() {
-        //Arrange
-        String id = "1";
+        String id = "5";
         String name = "Andrei";
         int group = 111;
-        int expected = 0;
 
         //Act
         int actual = service.saveStudent(id, name, group);
 
         //Assert
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(0, actual);
     }
 
     @Test
-    public void testSaveStudentTC9() {
-        //Arrange
+    public void testSaveAssignment() {
+        // Arrange
         String id = "1";
-        String name = "Andrei";
-        int group = 937;
-        int expected = 0;
+        String description = "some desc";
+        int deadline = 1;
+        int startline = 1;
 
-        //Act
-        int actual = service.saveStudent(id, name, group);
+        // Act
+        int result = service.saveAssignment(id, description, deadline, startline);
 
-        //Assert
-        Assertions.assertEquals(expected, actual);
+        // Assert
+        Assertions.assertEquals(0, result);
     }
 
     @Test
-    public void testSaveStudentTC10() {
+    public void testSaveGrade(){
         //Arrange
-        String id = "1";
-        String name = "Andrei";
-        int group = 937;
-        int expected = 0;
+        String idStudent = "1";
+        String idAssignment = "123";
+        int valGrade = 10;
+        int predata = 7;
+        String feedback = "Ok";
 
         //Act
-        int actual = service.saveStudent(id, name, group);
+        int actual = service.saveGrade(idStudent, idAssignment, valGrade, predata, feedback);
 
         //Assert
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(-1, actual);
     }
+
+    @Test
+    public void testIntegration(){
+        //Arrange
+        String idStudent = "5";
+        String name = "Andrei";
+        int group = 111;
+
+        String idAssignment = "1";
+        String description = "some desc";
+        int deadline = 1;
+        int startline = 1;
+
+        int valGrade = 10;
+        int predata = 7;
+        String feedback = "Ok";
+
+        //Act
+        int resStudent = service.saveStudent(idStudent, name, group);
+        int resAssignemnt = service.saveAssignment(idAssignment, description, deadline, startline);
+        int resGrade = service.saveGrade(idStudent, idAssignment, valGrade, predata, feedback);
+
+        //Assert
+        Assertions.assertEquals(0, resStudent);
+        Assertions.assertEquals(0, resAssignemnt);
+        Assertions.assertEquals(0, resGrade);
+    }
+
 }
